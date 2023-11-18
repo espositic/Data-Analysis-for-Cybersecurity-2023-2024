e2db024041d7839a4be3154cdaef21eef9cc0ea1
Matteo Esposito <matteo.esposito.14.2@gmail.com>
2023-11-18 17:29:13 +0100

    Aggiunte funzioni LAB3

--- funzioni.py
+++ funzioni.py
@@ -3,24 +3,21 @@ import numpy as np
 import os
 import matplotlib.pyplot as plt
 from sklearn.feature_selection import mutual_info_classif
+from sklearn.decomposition import PCA
 from tqdm import tqdm
 
 
 def load(path):
     """
-        Carica i dati da un file CSV in base al path indicato.
-        :param path: percorso del CSV da caricare
-        :return: dataframe contenente i dati
+    Carica i dati da un file CSV in base al path indicato.
     """
     return pd.read_csv(path)
 
 
 def preElaborationData(data, filename):
     """
-        Fornisce le statistiche degli attributi del dataset
-        I valori min,max, avg ecc sono salvati su un file csv
-        :param data: dataset
-        :param filename: nome del file csv in cui salvare i dati
+    Fornisce le statistiche degli attributi del dataset
+    I valori min,max, avg ecc sono salvati su un file csv
     """
     features_list = data.columns
     values = []
@@ -34,8 +31,6 @@ def preBoxPlotAnalysisData(X, Y):
     """
     Crea un box plot per ciascun attributo elencato nella lista raggrupati per classe
     Il box plot è salvato come immagine nella cartella boxplot
-    :param X: Dataframe X
-    :param Y: Dataframe Y
     """
     output_directory = 'boxplot'
 
@@ -59,9 +54,6 @@ def preBoxPlotAnalysisData(X, Y):
 def mutualInfoRank(X,Y,seed,output_file):
     """
     Calcola la mutualInfoClassif sulla lista delle variabili indipendenti
-    :param X: Dataframe X
-    :param Y: Dataframe Y
-    :return: lista di tuple (coppie nome/valore) ordinate in modo decrescente per mutual info
     """
     independentList = list(X.columns.values)
     mutual_info_values = []
@@ -72,9 +64,39 @@ def mutualInfoRank(X,Y,seed,output_file):
 
     results = list(zip(independentList, mutual_info_values))
     results.sort(key=lambda x: x[1], reverse=True)
-    
-    # Creazione di un DataFrame per i risultati
     result_df = pd.DataFrame(results, columns=['Nome', 'Valore'])
-    
-    # Salvataggio in un file CSV
     result_df.to_csv(output_file, index=False)
+    return result_df
+
+def topFeatureSelect(features,threshold):
+    '''
+    Seleziona le faeature con MI >= threshold
+    '''
+    selected_features = features[features['Valore'] >= threshold]
+    return selected_features['Nome'].tolist()
+
+def pca(dataframe):
+    '''
+    Calcola la PCA di X e restituisce la lista dei nomi e la lista delle explained variance delle pc 
+    '''
+    pca_model = PCA()
+    return pca_model.fit(dataframe), pca_model.components_, pca_model.explained_variance_ratio_
+
+def applyPCA(input_df, pca_model, pc_names):
+    '''
+    Applica la PCA al Dataframe
+    '''
+    transformed_data = pca_model.transform(input_df)
+    pc_df = pd.DataFrame(transformed_data, columns=pc_names)
+    return pc_df
+
+def numberOfTopPCSelect(explained_variance_ratio, threshold):
+    '''
+    Restituisce il numero delle prime PC le cui explained variance sommata è maggiore a threshold
+    '''
+    sum=0
+    i=0
+    while sum < threshold and i < len(explained_variance_ratio):
+        sum += explained_variance_ratio[i]
+        i += 1
+    return i
